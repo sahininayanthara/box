4760a6c985b6de3c9a1847d747b964d087a80f23
sahininayanthara <sahininayanthara95@gmail.com>
2019-02-21 08:21:37 +0530

    first commit

---
+++ src/box/Box.java
@@ -0,0 +1,24 @@
+
+package box;
+
+
+public class Box {
+
+    
+    public static void main(String[] args) {
+         int length =12;
+         int width =5;
+         int height=3;
+int volume =length*width*height;
+System.out.println("volume is " +volume);
+
+int area =2*length*height+2*height*width+2*length*width;
+System.out.println("area is " +area);
+
+
+        
+        
+        
+    }
+    
+}
